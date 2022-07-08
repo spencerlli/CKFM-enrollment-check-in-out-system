6edc1d1c8b7016e0a7ab2e79e8523220a30aa305
changliu020 <changliu020@gmail.com>
2022-07-07 20:15:30 -0700

    check in/out done

--- flask_code/app.py
+++ flask_code/app.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import pymysql
 pymysql.install_as_MySQLdb()
 
@@ -7,6 +8,8 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+from ast import literal_eval as make_tuple
+import random
 
 
 app = Flask(__name__)
@@ -18,6 +21,18 @@ app.config['MYSQL_PASSWORD'] = '123456'
 app.config['MYSQL_DB'] = 'Users'
 
 mysql = MySQL(app)
+
+
+def generate_random_str(randomLength=8):
+    """
+    生成一个指定长度的随机字符串
+    """
+    random_str = ''
+    base_str = 'ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz0123456789'
+    length = len(base_str) - 1
+    for i in range(randomLength):
+        random_str += base_str[random.randint(0, length)]
+    return random_str
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -41,8 +56,8 @@ def firstTimeEnroll():
         phoneNumber = request.form['phoneNumber']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM guardian WHERE phone_number = %s;', (phoneNumber, ))
-        account = cursor.fetchone()
-        if account:
+        guardian = cursor.fetchone()
+        if guardian:
             msg = 'Family already existed, please do additional enrollment.'
         # elif not (firstName and lastName):
         #     msg = 'Please fill out the form!'
@@ -76,42 +91,140 @@ def firstTimeEnroll():
 
 @app.route('/checkIn', methods=['GET', 'POST'])
 def checkIn():
-    msg = ''
-    if request.method == 'POST' and 'password' in request.form:
-        password = request.form['password']
+    msg = 'Input guardian\'s phone number to identify the family:'
+    if request.method == 'POST' and 'phoneNumber' in request.form:
+        phoneNumber = request.form['phoneNumber']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute(
-            'SELECT * FROM students WHERE password = %s', (password, ))
-        account = cursor.fetchone()
-        if account:
+            'SELECT * FROM guardian WHERE phone_number = %s;', (phoneNumber, ))
+        guardian = cursor.fetchone()
+        if guardian:
+            guardianId = guardian['id']
             cursor.execute(
-                "UPDATE students SET checked_in = true WHERE password = %s;", (password, ))
-            mysql.connection.commit()
-            msg = 'Check in successfully!'
-            return render_template('checkIn.html', msg=msg)
+                'SELECT * FROM family WHERE guardian_id = %s;', (guardianId, ))
+            familyId = cursor.fetchone()['id']
+            return redirect(url_for('checkInSelect', familyId=familyId))
         else:
-            msg = 'Incorrect password !'
+            msg = 'Incorrect phone number!'
+    elif request.method == 'POST':
+        msg = 'Please input guardian\'s phone number!'
     return render_template('checkIn.html', msg=msg)
+
+
+@app.route('/checkInSelect?<int:familyId>', methods=['GET', 'POST'])
+def checkInSelect(familyId=0):
+    if request.method == 'GET':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            'SELECT family.id, family.guardian_id, guardian.first_name, guardian.last_name, \
+                    family.student_id, student.first_name, student.last_name \
+            FROM family JOIN guardian ON family.guardian_id = guardian.id \
+                        JOIN student ON family.student_id = student.id \
+            WHERE family.id = %s;', (familyId, ))
+        data = cursor.fetchall()
+        print(data)
+
+        guardians, students = set(), set()
+        for d in data:
+            guardians.add((d['guardian_id'], d['first_name'], d['last_name']))
+            students.add((d['student_id'], d['student.first_name'], d['student.last_name']))
+
+        return render_template('checkInSelect.html', guardians=guardians, students=students, familyId=familyId)
+    else:
+        guardian = request.form['guardian']
+        student = request.form['student']
+        return redirect(url_for('checkInSuccess', guardian=guardian, student=student))
+
+
+@app.route('/checkInSuccess?<string:guardian>&<string:student>', methods=['GET'])
+def checkInSuccess(guardian, student):
+    guardian = make_tuple(guardian)
+    student = make_tuple(student)
+    guardianId = guardian[0]
+    studentId = student[0]
+    guardianName = guardian[1] + ' ' + guardian[2]
+    studentName = student[1] + ' ' + student[2]
+    barcode = generate_random_str(randomLength=8)
+
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(
+            'UPDATE student SET checked_in = %s, barcode = %s WHERE id = %s;', (guardianId, barcode, studentId, ))
+    mysql.connection.commit()
+
+    return render_template('checkInSuccess.html', guardianName=guardianName, studentName=studentName, barcode=barcode)
 
 
 @app.route('/checkOut', methods=['GET', 'POST'])
 def checkOut():
-    msg = ''
-    if request.method == 'POST' and 'password' in request.form:
-        password = request.form['password']
+    msg = 'Input guardian\'s phone number to identify the family:'
+    if request.method == 'POST' and 'phoneNumber' in request.form:
+        phoneNumber = request.form['phoneNumber']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute(
-            'SELECT * FROM students WHERE password = %s', (password, ))
-        account = cursor.fetchone()
-        if account:
+            'SELECT * FROM guardian WHERE phone_number = %s;', (phoneNumber, ))
+        guardian = cursor.fetchone()
+        if guardian:
+            guardianId = guardian['id']
             cursor.execute(
-                "UPDATE students SET checked_in = false WHERE password = %s;", (password, ))
-            mysql.connection.commit()
-            msg = 'Check Out successfully!'
-            return render_template('checkOut.html', msg=msg)
+                'SELECT * FROM family WHERE guardian_id = %s;', (guardianId, ))
+            familyId = cursor.fetchone()['id']
+            return redirect(url_for('checkOutSelect', familyId=familyId))
         else:
-            msg = 'Incorrect password!'
+            msg = 'Incorrect phone number!'
+    elif request.method == 'POST':
+        msg = 'Please input guardian\'s phone number!'
     return render_template('checkOut.html', msg=msg)
+
+
+@app.route('/checkOutSelect?<int:familyId>', methods=['GET', 'POST'])
+def checkOutSelect(familyId=0):
+    if request.method == 'GET':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            'SELECT family.id, family.guardian_id, guardian.first_name, guardian.last_name, \
+                    family.student_id, student.first_name, student.last_name \
+            FROM family JOIN guardian ON family.guardian_id = guardian.id \
+                        JOIN student ON family.student_id = student.id \
+            WHERE family.id = %s;', (familyId, ))
+        data = cursor.fetchall()
+
+        guardians, students = set(), set()
+        for d in data:
+            guardians.add((d['guardian_id'], d['first_name'], d['last_name']))
+            students.add((d['student_id'], d['student.first_name'], d['student.last_name']))
+
+        return render_template('checkOutSelect.html', guardians=guardians, students=students, familyId=familyId)
+    else:
+        guardian = request.form['guardian']
+        student = request.form['student']
+        studentId = make_tuple(student)[0]
+        barcode = request.form['barcode']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            'SELECT id from student where barcode = %s;', (barcode, ))
+        if cursor.fetchone()['id'] == studentId:
+            return redirect(url_for('checkOutSuccess', msg=None, guardian=guardian, student=student))
+        else:
+            msg = 'Picking up wrong student, please verify.'
+            return render_template('checkOutSelect.html', msg=msg, guardians=guardians, students=students, familyId=familyId)
+
+
+@app.route('/checkOutSuccess?<string:guardian>&<string:student>', methods=['GET'])
+def checkOutSuccess(guardian, student):
+    guardian = make_tuple(guardian)
+    student = make_tuple(student)
+    guardianId = guardian[0]
+    studentId = student[0]
+    guardianName = guardian[1] + ' ' + guardian[2]
+    studentName = student[1] + ' ' + student[2]
+
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(
+            'UPDATE student SET checked_in = 0, checked_out = %s, barcode = NULL WHERE id = %s;', (guardianId, studentId, ))
+    mysql.connection.commit()
+
+    return render_template('checkOutSuccess.html', guardianName=guardianName, studentName=studentName)
 
 
 # @app.route('/login', methods=['GET', 'POST'])
