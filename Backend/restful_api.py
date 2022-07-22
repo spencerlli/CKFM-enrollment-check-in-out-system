95ef55b5b7eb50aa8ef4e581f97b8dac46a1fac3
changliu020 <changliu020@gmail.com>
2022-07-21 20:06:37 -0700

    restful api port change

--- Backend/restful_api.py
+++ Backend/restful_api.py
@@ -209,4 +209,4 @@ api.add_resource(StudentListResource, '/student')
 api.add_resource(StudentResource, '/student/<int:id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
